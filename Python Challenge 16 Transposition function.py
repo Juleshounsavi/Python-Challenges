fa60ba3dee613afc7035e980cddc94ad360b842d
HOUNSAVI Jules koffi <114362041+Juleshounsavi@users.noreply.github.com>
2024-06-14 21:37:47 +0100

    Add files via upload

---
+++ Python Challenge 16 Transposition function.py
@@ -0,0 +1,19 @@
+#A function that display the transposition of a matrix entered by the user.
+
+def Transpo(A, row, col):
+    B = [[0 for _ in range(row)] for _ in range(col)]
+    for i in range(row):
+        for j in range(col):
+            B[j][i] = A[i][j]
+    print("Affichage de ma matrice A:")
+    for li in A:
+        print(li)
+    print("\nAffichage de ma matrice B transposee de A:")
+    for li in B:
+        print(li)  
+
+Mat = [[1, 2, 3], 
+       [4, 5, 6]]
+
+
+Transpo(Mat, 2, 3)
