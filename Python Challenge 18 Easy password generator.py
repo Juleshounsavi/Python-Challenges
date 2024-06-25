4602c374c9cdee0914c7bd118b23c9db3310b36c
HOUNSAVI Jules koffi <114362041+Juleshounsavi@users.noreply.github.com>
2024-06-25 22:03:55 +0100

    Update Python Challenge 18 Easy password generator.py

--- Python Challenge 18 Easy password generator.py
+++ Python Challenge 18 Easy password generator.py
@@ -4,7 +4,7 @@ import random
 def EasyGenerate():
     passw = []
     for i in range(0,8):
-        passw.append(str(random.randint(0,9)))
+        passw.append(int(random.randint(0,9)))
     return passw
     
 one = EasyGenerate()
