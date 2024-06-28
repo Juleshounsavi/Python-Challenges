f60a9a976e55b4c9ec6e4ed0c6100aa28f4f0ba6
HOUNSAVI Jules koffi <114362041+Juleshounsavi@users.noreply.github.com>
2024-06-28 23:42:23 +0100

    Add files via upload

---
+++ Python Challenge 19 Math functions plot 1.py
@@ -0,0 +1,6 @@
+#Plots of some math functions using python libraries.
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+from math import cos, sin, tan, log, exp, sqrt, pi
