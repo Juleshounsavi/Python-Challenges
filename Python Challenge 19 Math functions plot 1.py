e6f39363c26349e180a05b186edf276220afaf96
HOUNSAVI Jules koffi <114362041+Juleshounsavi@users.noreply.github.com>
2024-06-29 17:27:30 +0100

    Delete Python Challenge 19 Math functions plot 1.py

--- Python Challenge 19 Math functions plot 1.py
+++
@@ -1,6 +0,0 @@
-#Plots of some math functions using python libraries.
-import numpy as np
-import pandas as pd
-import matplotlib.pyplot as plt
-import seaborn as sns
-from math import cos, sin, tan, log, exp, sqrt, pi
