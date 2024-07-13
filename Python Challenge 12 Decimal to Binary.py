f1866d546f5c882eee0306179b2a0ac214f63155
HOUNSAVI Jules koffi <114362041+Juleshounsavi@users.noreply.github.com>
2024-07-13 23:30:02 +0100

    Update Python Challenge 12 Decimal to Binary.py

--- Python Challenge 12 Decimal to Binary.py
+++ Python Challenge 12 Decimal to Binary.py
@@ -3,10 +3,8 @@
 def Decimal_to_binary(x):
     if x >= 1024 or x < 0:
         raise ValueError("The decimal number must be between 0 and 1023 inclusive.")
-    
     #Convert the decimal number to binary and remove the '0b' prefix
     binary_representation = bin(x)[2:]
-
     #Pad the binary representation with leading zeros to make it 10 digits long
     binary_representation = binary_representation.zfill(10)
 
