6632632992aa36d4c21e3e60f336d1e49c747e17
HOUNSAVI Jules koffi <114362041+Juleshounsavi@users.noreply.github.com>
2024-08-06 21:30:18 +0100

    Update Python Challenge 15 Second largest number.py

--- Python Challenge 15 Second largest number.py
+++ Python Challenge 15 Second largest number.py
@@ -1,9 +1,10 @@
 #A function that print the second largest number of a list.
 
 def Second_large_number_finder(liste):    
-    x = max(liste)
+    liste_cp = liste.copy()
+    x = max(liste_cp)
     liste.remove(x)
-    y = max(liste)
+    y = max(liste_cp)
     print("The second largest number is: {}".format(y))
 
 
