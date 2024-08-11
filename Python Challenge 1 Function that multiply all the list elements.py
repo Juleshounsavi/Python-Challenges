c9ebfbe2f8a25d0ccb6778225b27159b861a1b96
HOUNSAVI Jules koffi <114362041+Juleshounsavi@users.noreply.github.com>
2024-08-11 22:57:08 +0100

    Update Python Challenge 1 Function that multiply all the list elements.py

--- Python Challenge 1 Function that multiply all the list elements.py
+++ Python Challenge 1 Function that multiply all the list elements.py
@@ -1,14 +1,12 @@
 #Fonction which multiply all the numbers of a list.
 
 n=int(input("Enter the lengh of your list:"))
-
 liste = []
 
 for i in range(0, n):
     x=int(input(("Enter element:")))
     liste.append(x)
 print(liste) 
-
 
 def MultiplyListeElements(liste_x):
     result=1
